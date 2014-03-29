42ef1f8f879fd480a6dbe06fefd0d6e1b07dce8a
Michael Vitz <vitz.michael@gmail.com>
2014-03-29 13:52:19 +0100

    Unit test for FizzBuzz is now parameterized.

--- src/test/java/de/mvitz/kata/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/de/mvitz/kata/fizzbuzz/FizzBuzzTest.java
@@ -1,19 +1,37 @@
 package de.mvitz.kata.fizzbuzz;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class FizzBuzzTest {
 
-    @Test
-    public void testFizzBuzzOfOne() throws Exception {
-        assertThat(FizzBuzz.of(1)).as("FizzBuzz of 1").isEqualTo("1");
+    @Parameters
+    public static Collection<Object[]> parameters() {
+        return Arrays.asList(
+                new Object[] { 1, "1" },
+                new Object[] { 3, "Fizz" }
+        );
+    }
+
+    private final int number;
+    private final String expected;
+
+    public FizzBuzzTest(int number, String expected) {
+        this.number = number;
+        this.expected = expected;
     }
 
     @Test
-    public void testFizzBuzzOfThree() throws Exception {
-        assertThat(FizzBuzz.of(3)).as("FizzBuzz of 3").isEqualTo("Fizz");
+    public void testFizzBuzzOf() throws Exception {
+        assertThat(FizzBuzz.of(number)).as("FizzBuzz of %s", number).isEqualTo(expected);
     }
 
 }
